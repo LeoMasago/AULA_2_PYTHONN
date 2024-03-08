312596fe0f36bd36990b89d8be30c3fe0b782ec8
LeoMasago <leo.masago@etec.sp.gov.br>
2024-03-08 11:31:24 -0300

    Faltando somente ex 5 e 9

--- main.py
+++ main.py
@@ -171,6 +171,61 @@ else:
     resultado = 0.25 * compradas
     print(f"{resultado}")
 
-'''
 
-# Exercício
+
+# Exercício 5
+
+# Exercício 6
+
+altura = float(input("Digite sua altura: "))
+sexo = input("Digite seu sexo: ")
+if sexo == 'm':
+    ideal = (72.7 * altura) - 58
+    print(f"Com {altura} de altura, você deve pesar {ideal}")
+elif sexo == 'f':
+    ideal = (62.1 * altura) - 44.7
+    print(f"Com {altura} de altura, você deve pesar {ideal}")
+
+
+# Exercício 7 e 8
+lados = int(input("Digite a quantidade de lados: "))
+medida = int(input("Digite as medidas em cm: "))
+if lados < 3 or lados > 5:
+    print("Polígono não identificado")
+elif lados == 3:
+    area = lados * medida
+    print(f"A área desse Triângulo é de {area} cm")
+elif lados == 4:
+    area = lados * medida
+    print(f"A área desse Quadrado é de {area} cm")
+elif lados == 5:
+    area = lados * medida
+    print(f"A área desse Pentágono é de {area} cm")
+
+
+# Exercício 9
+
+# Exercício 10
+
+lado1 = int(input("Digite a medida de um triângulo: "))
+lado2 = int(input("Digite outra medida: "))
+lado3 = int(input("Digite a última medida: "))
+if lado1 == lado2 == lado3:
+    print("Este triângulo é Equilátero")
+elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
+    print("Este triângulo é Isóscele")
+elif lado1 != lado2 != lado3:
+    print("Este triângulo é Escaleno")
+
+# Exercício 11
+a1 = int(input("Digite um ângulo: "))
+a2 = int(input("Digite outro ângulo: "))
+a3 = int(input("Digite o último ângulo: "))
+soma = a1 + a2 + a3
+if soma == 90:
+    print(f"{soma}º é um Triângulo Retângulo")
+elif soma > 90:
+    print(f"{soma}º é um Triângulo Obtusângulo")
+else:
+    print(f"{soma}º é um Triângulo Acutângulo")
+'''
